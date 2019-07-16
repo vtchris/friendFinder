6cb2ec2de728e2db6bd4a1cbdb07568a59b3fc6b
VTC <vtchris@hotmail.com>
2019-07-16 11:52:44 -0400

    Basic post from survey.html to server

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,7 +3,8 @@ module.exports = function(app){
     app.get('/api/friends',function(req,res){
         
     })
-    app.post('/api/friends',function(){
+    app.post('/api/friends',function(req,res){
+        console.log(req.body)
 
     })
    
--- server.js
+++ server.js
@@ -3,9 +3,17 @@ var express = require("express");
 
 var PORT = process.env.PORT || 3000;
 
+
+
 var app = express();
-require("./app/routing/htmlRoutes.js")(app);
-require("./app/routing/apiRoutes.js")(app);
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
 
 
 
